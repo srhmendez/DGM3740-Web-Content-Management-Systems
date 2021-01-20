6ba4cefbbc4036405d19b52f669538a369939bca
srhmendez <srhmendez@gmail.com>
2021-01-20 14:19:03 -0700

    Created Carousel

--- Cabot_Cruises/main.js
+++ Cabot_Cruises/main.js
@@ -13,48 +13,19 @@ var x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
 
 
+
 $(document).ready(function(){
-    $('.hero-large').slick({
-      autoplay: true;
+    $('.heroCaro').slick({
+        dots:true,
+        arrows:false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        cssEase: 'ease-out',
+        autoplay:true,
+        autoplaySpeed:30000
     });
   });
 
-//display one image at a time
-$('.single-item').slick();
 
-//Making the Carousel Responsive
-$('.responsive').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
