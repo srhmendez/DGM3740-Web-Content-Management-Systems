e6f9ea9f9d8a3fed1a492153b2be0dd2f128a839
srhmendez <srhmendez@gmail.com>
2021-02-25 00:13:38 -0700

    starting to structure out cruises page

--- Modularization/cruises.php
+++ Modularization/cruises.php
@@ -2,7 +2,48 @@
 <?php include"_header.php";?>
 
 
+<section id="Cruises">
+<h1>Cruises </h1>
+<div class="cruise-booking-container">
+    <div class="cruise-booking">
+        <img src="/Images/Cruise-boat-1.jpg" alt="Large Cruise ship over sailing across open sea">
+        <div class="cruise-text">
+            <h2 class="cruise-h2">Norweigan Cruises now 30% off if you bundle your flight and hotel!</h2>
+            <p class="cruise-p">A bunch of random text about cruise lines and how to get a great deal on Norweigan cruises today.</p>
+            <button>Read More</button>
+        </div>
+    </div>
 
+
+    <div class="cruise-booking">
+        <img src="/Images/Cruise-boat-2.jpg" alt="Large Cruise ship over sailing across open sea">
+        <div class="cruise-text">
+            <h2 class="cruise-h2">Norweigan Cruises now 30% off if you bundle your flight and hotel!</h2>
+            <p class="cruise-p">A bunch of random text about cruise lines and how to get a great deal on Norweigan cruises today.</p>
+            <button>Read More</button>
+        </div>
+    </div>
+
+    <div class="cruise-booking">
+        <img src="/Images/Cruise-boat-3.jpg" alt="Large Cruise ship over sailing across open sea">
+        <div class="cruise-text">
+            <h2 class="cruise-h2">Norweigan Cruises now 30% off if you bundle your flight and hotel!</h2>
+            <p class="cruise-p">A bunch of random text about cruise lines and how to get a great deal on Norweigan cruises today.</p>
+            <button>Read More</button>
+        </div>
+    </div>
+
+    <div class="cruise-booking">
+        <img src="/Images/Cruise-boat-4.jpg" alt="Large Cruise ship over sailing across open sea">
+        <div class="cruise-text">
+            <h2 class="cruise-h2">Norweigan Cruises now 30% off if you bundle your flight and hotel!</h2>
+            <p class="cruise-p">A bunch of random text about cruise lines and how to get a great deal on Norweigan cruises today.</p>
+            <button>Read More</button>
+        </div>
+    </div>
+</div>
+
+</section>
 
 <!-- INCLUDE FOOTER -->
 <?php include"_footer.php";?>
