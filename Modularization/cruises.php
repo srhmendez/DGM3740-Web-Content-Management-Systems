bafaca39a599ec952fde30731c8b243d0a5eeecf
srhmendez <srhmendez@gmail.com>
2021-03-01 17:14:16 -0700

    creating Cruises Page

--- Modularization/cruises.php
+++ Modularization/cruises.php
@@ -1,49 +1,77 @@
 <!-- INCLUDE HEADER -->
 <?php include"_header.php";?>
 
+    <section id="Cruises">
 
-<section id="Cruises">
-<h1>Cruises </h1>
-<div class="cruise-booking-container">
-    <div class="cruise-booking">
-        <img class="cruise-booking" src="Images/Cruise-boat-1.jpg" alt="Large Cruise ship over sailing across open sea">
-        <div class="cruise-text">
-            <h2 class="cruise-h2">Norweigan Cruises now 30% off if you bundle your flight and hotel!</h2>
-            <p class="cruise-p">A bunch of random text about cruise lines and how to get a great deal on Norweigan cruises today.</p>
-            <button>Read More</button>
+
+        <h1 id="cruises">Cruises </h1>
+
+
+        <div class="cruise-booking-container">
+            <figure class="cruise-booking">
+                <img class="cruise-booking" src="Images/Cruise-boat-1.jpg"
+                    alt="Large Cruise ship over sailing across open sea">
+                <div class="cruise-text">
+                    <h2 class="cruise-h2">Australia & New Zealand Cruises from $999!</h2>
+                    <p class="cruise-p">On the other side of the world, Australia and New
+                        Zealand are full of wonders to behold! Now, more
+                        than ever before, you can get phenomenal
+                        discounts to cruise the South Pacific. Sail to New
+                        Zealand from Sydney, Australia on a 12-day cruise
+                        from just $888 per person plus taxes.</p>
+                    <button>Read More</button>
+                </div>
+            </figure>
+
+
+            <figure class="cruise-booking">
+                <img class="cruise-booking" src="Images/Cruise-boat-2.jpg"
+                    alt="Large Cruise ship over sailing across open sea">
+                <div class="cruise-text">
+                    <h2 class="cruise-h2">Norweigan Cruises from $1099 if you bundle your flight and hotel!</h2>
+                    <p class="cruise-p">Our award-winning ships are designed for you to enjoy as you choose while
+                        cruising to beautiful destinations around the world. Add a wide range of superb dining options
+                        and spectacular entertainment and you’ll experience your best vacation ever.</p>
+                    <button>Read More</button>
+                </div>
+            </figure>
+
+            <figure class="cruise-booking">
+                <img class="cruise-booking" src="Images/Cruise-boat-3.jpg"
+                    alt="Large Cruise ship over sailing across open sea">
+                <div class="cruise-text">
+                    <h2 class="cruise-h2">Lose yourself in a Carribean Paradise for $2000.</h2>
+                    <p class="cruise-p">Set sail on a Caribbean cruise and discover the cliff-diving, breeze-swaying,
+                        sand-between-your-toes, no-worries pace of island life. Hundreds of years of history have left
+                        jungle ruins from ancient times and vibrant colonial towns with brightly painted buildings
+                        reflecting a fascinating history. </p>
+                    <button>Read More</button>
+                </div>
+            </figure>
+
+            <figure class="cruise-booking">
+                <img class="cruise-booking" src="Images/Cruise-boat-4.jpg"
+                    alt="Large Cruise ship over sailing across open sea">
+                <div class="cruise-text">
+                    <h2 class="cruise-h2">Mediterranean Cruises starting at $4000 are just a click away.</h2>
+                    <p class="cruise-p">All of our cruise itineraries offer just the right balance of action-packed days
+                        in port with relaxing days at sea, plus extended hours and overnight stays in select cities. We
+                        also include more overnights at the beginning and end of our cruises, so you can spend an extra
+                        day exploring cities like Barcelona, Venice and Rome, and use the ship as your hotel.</p>
+                    <button>Read More</button>
+                </div>
+            </figure>
         </div>
-    </div>
+
+    </section>
+
+</body>
+
+</html>
 
 
-    <div class="cruise-booking">
-        <img class="cruise-booking" src="Images/Cruise-boat-2.jpg" alt="Large Cruise ship over sailing across open sea">
-        <div class="cruise-text">
-            <h2 class="cruise-h2">Norweigan Cruises now 30% off if you bundle your flight and hotel!</h2>
-            <p class="cruise-p">A bunch of random text about cruise lines and how to get a great deal on Norweigan cruises today.</p>
-            <button>Read More</button>
-        </div>
-    </div>
 
-    <div class="cruise-booking">
-        <img class="cruise-booking" src="Images/Cruise-boat-3.jpg" alt="Large Cruise ship over sailing across open sea">
-        <div class="cruise-text">
-            <h2 class="cruise-h2">Norweigan Cruises now 30% off if you bundle your flight and hotel!</h2>
-            <p class="cruise-p">A bunch of random text about cruise lines and how to get a great deal on Norweigan cruises today.</p>
-            <button>Read More</button>
-        </div>
-    </div>
 
-    <div class="cruise-booking">
-        <img class="cruise-booking" src="Images/Cruise-boat-4.jpg" alt="Large Cruise ship over sailing across open sea">
-        <div class="cruise-text">
-            <h2 class="cruise-h2">Norweigan Cruises now 30% off if you bundle your flight and hotel!</h2>
-            <p class="cruise-p">A bunch of random text about cruise lines and how to get a great deal on Norweigan cruises today.</p>
-            <button>Read More</button>
-        </div>
-    </div>
-</div>
-
-</section>
 
 <!-- INCLUDE FOOTER -->
 <?php include"_footer.php";?>
