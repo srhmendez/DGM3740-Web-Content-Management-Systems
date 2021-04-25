eafa25e800f64600a32d213751137231b7244eee
Sarah Mendez <srhmendez@gmail.com>
2021-04-25 16:19:53 -0600

    fixing structure to work on wp

--- Dreamer-Vacations/wp-content/themes/dreamervacationstheme/footer.php
+++ Dreamer-Vacations/wp-content/themes/dreamervacationstheme/footer.php
@@ -1,68 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dreamer Vacations</title>
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <!-- Bootstrap Style Sheet Link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <!-- Custom Style Sheet Link -->
-    <link rel="stylesheet" href="/CSS/style.css">
-</head>
-<body>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><i class="fas fa-anchor"></i></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/contact.html">Contact Us</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a href="#" class="nav-link">
-                  Resorts
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a href="#" class="nav-link">
-                  Agents
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a href="#" class="nav-link">
-                  Packages
-                </a>
-              </li>             
-
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-
-    
-
-
-    
-    
-
-
-
       <!-- Footer -->
-    <section class="footer">
+    </main>
+      <section class="footer">
     <footer>
         <div id="footerContainer">
 
@@ -115,3 +53,5 @@
     <script src="/JS/main.js"></script>
 </body>
 </html>
+
+<!-- End of Footer -->
