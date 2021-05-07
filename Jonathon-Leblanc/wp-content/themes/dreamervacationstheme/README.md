8ec84c557b49d1460cec1428678a270421cf3bca
Sarah Mendez <srhmendez@gmail.com>
2021-05-06 22:28:35 -0600

    adding new folder for final

---
+++ Jonathon-Leblanc/wp-content/themes/dreamervacationstheme/README.md
@@ -0,0 +1,36 @@
+# BOOTSTRAP CSS THEME FOR DREAMER VACATIONS
+
+## Install WordPress
+Install another instance of WordPress in a new sub-domain for Dreamer Vacations LLC.
+HTML Template
+Build a WordPress theme based on a CSS framework of your choice. It should contain all the normal pieces of a website
+
+[x] - Logo
+[x] - Company name
+[x] - Search bar
+[x] - Responsive menu
+[x] - Footer with admin link
+[x] - Social Media links
+
+
+## Custom Theme
+
+[x] - Convert your html page into a custom WordPress theme
+[x] - Logo
+[x] - Company name
+[x] - Search bar
+[x] - Responsive menu
+[x] - Footer with admin link
+[x] - Social Media links
+
+
+
+## Pages
+[x] - Build 4 pages for Dreamer Vacations LLC that show off the CSS framework you selected. Here are some ideas.
+
+Contact Us page
+Resorts page
+Popular Destinations page
+Agents page
+Partners page
+Bargin Trips page
---
+++ Jonathon-Leblanc/wp-content/themes/dreamervacationstheme/footer.php
@@ -0,0 +1,57 @@
+      <!-- Footer -->
+    </main>
+      <section class="footer">
+    <footer>
+        <div id="footerContainer">
+
+            <div class="address">
+                <h5>Address</h5>
+                <a class="addressLink" href="https://goo.gl/maps/8xjqcKbDYvaf6H7e7"></a><p class="footerText">Cabot Cruises</p>
+                <p class="footerText">150 Civic Center Dr</p>
+                <p class="footerText">Sandy, UT 84070</p>
+                <h5>Phone</h5>
+                <p class="footerText">(555)-555-5555</p>
+                <h5>Hours of Operation</h5>
+                <p class="footerText">Monday-Friday: 8am - 7pm</p>
+            </div>
+            <div class="newsletter">
+                <h5>Newsletter</h5>
+                <p class="footerText"> Subscribe to our Newsletter to stay up to date with our hottest deals and limited packages.</p>
+                    <div id="input2">
+                        <form>
+                            <input class="form-control" type="email" placeholder="email address" id="form2">
+                        </form>
+                        <button class="subscribe" type="submit" value="Submit">Subscribe</button>
+                    </div>
+            </div>
+            <div class="why">
+                <h5>Why Dreamer Vacations?</h5>
+                <p class="footerText" id="cabotText">Cabot Cruises is a travel agency providing the best cruise deals on our online travel website. We can help you with all inclusive vacations
+                    including discounted cruises.
+                    We do not sell travel to residents of Delaware, Iowa, Florida, Hawaii and Washington state because those states are just weird. If you are a
+                    resident of one of these states, call your congressman and tell them to change the regulations.</p>
+            </div>
+        </div>
+        <p class="footerTextn adminLink"><a href="https://dgm3740.sarahmendez.com/Dreamer-Vacations/wp-login.php">Admin Link</a></p>
+        <p class="copyright">&#169; 2021 &sdot; Sarah Mendez &sdot; Login</p>
+        <div class="social">
+            <div class="iconContainer">
+                <i class="fab fa-facebook-f social"></i>
+                <i class="fab fa-twitter social"></i>
+                <i class="fab fa-instagram social"></i>
+                <i class="fab fa-linkedin social"></i>
+                <i class="fab fa-youtube social"></i>
+            </div>
+        </div>
+    </footer>
+    </section>
+
+
+    <!-- Bootstrap JS Link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <!-- Custom JS Link -->
+    <script src="/JS/main.js"></script>
+</body>
+</html>
+
+<!-- End of Footer -->
